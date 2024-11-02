0a67c1b430263bf3a795981443fa2807e3c47f01
makennawold <makennawold@gmail.com>
2024-11-02 12:28:05 -0600

    added local

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TodoList from "./components/TodoList"
 import TodoInput from "./components/TodoInput"
 
 function App() {
-  const [todos, setTodos] = useState([
-    'gymin', 
-    'dont wanna',
-    'work'
-  ])
+  const [todos, setTodos] = useState([])
   const [todoValue, setTodoValue] = useState('')
+
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({todos: newList}))
+  }
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
     setTodos(newTodoList)
+    persistData(newTodoList)
   }
 
   function handleDeleteTodo(index) {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index
     })
+    persistData(newTodoList)
     setTodos(newTodoList)
   } 
 
@@ -28,6 +30,20 @@ function App() {
     setTodoValue(valueToBeEdited)
     handleDeleteTodo(index)
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  }, [])
 
   return (
     <main>
